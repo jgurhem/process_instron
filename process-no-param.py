59d6f765f5d5a9f98629ed642ece223575a8d1e3
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-12-14 21:24:28 +0100

    Add app without params

---
+++ process-no-param.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os, fnmatch
+import sys
+import csv
+from core.curves import make_curve
+from core.compute_params import find_point
+
+def main():
+  if len(sys.argv) != 2:
+    print("Usage : ")
+    print("py", sys.argv[0], "directory")
+    sys.exit(0)
+  nbCoeff = 6
+  directory = sys.argv[1] #directory from which the script is executed
+  data_curves = dict()
+  for file in fnmatch.filter(os.listdir(directory), '*.csv'):
+    processRawFile(directory + "/" + file, nbCoeff, data_curves)
+  make_curve(data_curves, directory)
+
+def processRawFile(file, nbCoeff, data_curves):
+  data = np.genfromtxt( file , usecols =(5, 6), delimiter=";", skip_header=2, encoding="latin1", dtype=None)
+
+  print(data)
+
+  depla = []
+  chargeN = []
+  lissage = []
+  PPmax = []
+  aire = []
+
+  for i, el in enumerate(data):
+    depla.append(float(el[0].replace(',','.')))
+    chargeN.append(float(el[1].replace(',','.')))
+
+  for i in range(nbCoeff):
+    lissage.append(0)
+    PPmax.append(0)
+    aire.append(0)
+
+  div = 0
+  for i in range(1, nbCoeff+1):
+    div += i * 2
+
+  PMax = 0
+  PMax_index = 0
+  for i in range(nbCoeff, len(depla) - nbCoeff):
+    tmp = 0
+    for j in range(1, nbCoeff+1):
+      #print(i, j, nbCoeff - j + 1, chargeN[i - j], chargeN[i + j])
+      tmp += (chargeN[i - j] + chargeN[i + j])*(nbCoeff - j + 1)
+    tmp = tmp/div
+    if tmp > PMax:
+      PMax = tmp
+      PMax_index = i
+    lissage.append(tmp)
+
+
+  for i in range(nbCoeff, len(lissage)):
+    tmp = lissage[i]*100/PMax
+    PPmax.append(tmp)
+
+  for i in range(nbCoeff, len(lissage) - 1):
+    tmp = (lissage[i] + lissage[i+1]) / 2 * (depla[i+1] - depla[i])
+    aire.append(tmp)
+
+  ra = find_point(0, len(lissage), lissage, depla, (lambda i: PPmax[i] > 50))
+  rb = find_point(0, len(lissage), lissage, depla, (lambda i: PPmax[i] > 30))
+  print(ra, rb)
+  KBond = (rb["y"] - ra["y"]) / (rb["x"] - ra["x"])
+
+  ra = find_point(ra["index"], len(lissage), lissage, depla, (lambda i: PPmax[i] < 50))
+  rb = find_point(ra["index"], len(lissage), lissage, depla, (lambda i: PPmax[i] < 30))
+  print(ra, rb)
+  KDbond = (rb["y"] - ra["y"]) / (rb["x"] - ra["x"])
+  KDbond_p = ra["y"] - ra["x"] * KDbond
+  KDbond_y0 = - KDbond_p / KDbond
+
+  d1=0
+  d2=0
+  d3=0
+  incr=0
+
+  for i in range(incr, len(lissage)):
+    if lissage[i] > PMax/2:
+      d1 = depla[i]
+      incr = i
+      break
+
+  for i in range(incr, len(lissage)):
+    if lissage[i] == PMax:
+      d2 = depla[i]
+      incr = i
+      borne2 = i
+      break
+
+  PMin = min(chargeN[borne2:min(2*borne2, len(chargeN))])
+  PMaxFil = max(chargeN[borne2:len(chargeN)])
+  for i in range(incr, len(lissage)):
+    if lissage[i] < PMax/2:
+      d3 = depla[i]
+      break
+
+  s1=2*(d2 - d1)
+  s2=2*(d3 - d2)
+
+  borne1=0
+  borne3=0
+
+  for i in range(len(lissage)):
+    if depla[i] > d2 - s1:
+      borne1 = i
+      break
+
+  for i in range(borne2, len(lissage)):
+    if depla[i] > d2 + s2:
+      borne3 = i
+      break
+
+  aire1=0
+  aire2=0
+  for i in range(borne1, borne2):
+    aire1 += aire[i]
+  for i in range(borne2, borne3):
+    aire2 += aire[i]
+
+  fout = open(os.path.splitext(file)[0] + ".csv.out", 'w')
+
+  fout.write('PMax;' + str(PMax) + "\n")
+  fout.write('Kbond;' + str(KBond) + "\n")
+  fout.write('Kdebond;' + str(KDbond) + "\n")
+  fout.write('Kdebond_y0;' + str(KDbond_y0) + "\n")
+  fout.write('aire1;' + str(aire1) + "\n")
+  fout.write('aire2;' + str(aire2) + "\n")
+  fout.write('s1;' + str(s1) + "\n")
+  fout.write('s2;' + str(s2) + "\n")
+  fout.write('d1;' + str(d1) + "\n")
+  fout.write('d2;' + str(d2) + "\n")
+  fout.write('d3;' + str(d3) + "\n")
+  fout.write('PMin;' + str(PMin) + "\n")
+  fout.write('PMaxFil;' + str(PMaxFil) + "\n")
+  computed_params = dict()
+  computed_params["PMax"] = PMax
+  computed_params["Kbond"] = KBond
+  computed_params["Kdebond"] = KDbond
+  computed_params["PMin"] = PMin
+  computed_params["aire1"] = aire1
+  computed_params["aire2"] = aire2
+  computed_params["s1"] = s1
+  computed_params["s2"] = s2
+  computed_params["d1"] = d1
+  computed_params["d2"] = d2
+  computed_params["d3"] = d3
+
+
+  units = dict()
+  units["PMax"] = "N"
+  units["Kbond"] = "N/mm"
+  units["Kdebond"] = "N/mm"
+  units["nKRes"] = "N/mm"
+  units["nPRes"] = "N"
+  units["aire1"] = "mm*mm"
+  units["aire2"] = "mm*mm"
+  units["depla0"] = "mm"
+  units["ext_mass"] = "g"
+  units["s1"] = "mm"
+  units["s2"] = "mm"
+  units["d1"] = "mm"
+  units["d2"] = "mm"
+  units["d3"] = "mm"
+  units["ext_mass_on_depla0"] = "g/mm"
+
+
+  for i in range(len(aire)):
+    fout.write('{};{};{};{};{}\n'.format(depla[i], chargeN[i], lissage[i], PPmax[i], aire[i]))
+  fout.close()
+  data_curves[file] = (depla, lissage, computed_params, units)
+
+main()
